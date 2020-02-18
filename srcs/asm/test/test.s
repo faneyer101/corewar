394fe70e187399addc0aae5202026bbbb7ad71a7
Fabien Neyer <faneyer@z4r3p2.le-101.fr>
2020-02-18 20:16:24 +0100

    push lexer/parser presque ok

--- srcs/asm/test/test.s
+++ srcs/asm/test/test.s
@@ -1,23 +1,35 @@
-.name "zork"
-.comment "I'M ALIIIIVE"
-
 #
 # Warrior qui a gagne
 # l'annee derniere
 #     VOALA
 #
-#.name		"Celebration Funebre v0.99pl42"
-#.comment	"Jour J"
+.name		"Celebration Funebre v0.99pl42"
+.comment	"Jour J"
 
-		ld   %0,r3		
+		ld   %0,r3
 label:		zjmp %:avantdebt
 		zjmp %0				# pour eviter ecrasement
 						# de l'instruction suivante
-code:
-
-
-#l2:		sti r1, %:live, %1
-#		and r1, %0, r1
-#
-#live:	live %1
-#		zjmp %:live
+code:		live %42			# 5
+		sti  r15,%0,r14			# 6
+		zjmp %-200			# 3
+		live %42			# 5
+		sti  r15,%0,r14			# 6
+		zjmp %-200			# 3
+		live %42			# 5
+		sti r15,%0,r14			# 6
+		zjmp %-200			# 3
+		live %42			# 5
+		sti  r15,%0,r14			# 6
+		zjmp %-200			# 3
+		sti  r15,%0,r14			# 6
+		zjmp %-200			# 3
+		sti  r15,%0,r14			# 6
+		zjmp %-200
+	
+avantdebt:	sti   r1,%:code,%1  # 		# mets a jour le live
+		sti   r1,%:code,%15 # 		# mets a jour le live
+		sti   r1,%:code,%29 # 		# mets a jour le live
+		sti   r1,%:code,%43 # 		# mets a jour le live
+		sti   r1,%:code,%57 # 		# mets a jour le live
+		sti   r1,%:code,%71 # 		# mets a jour le live
