50df25f1b75955a79cc9ddce3d4fb336dd487a83
Nicolas Salle <nsalle@z4r7p5.le-101.fr>
2020-02-19 13:50:05 +0100

    Arena

--- includes/vm.h
+++ includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: nsalle <nsalle@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/10 12:13:30 by nsalle       #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/11 16:41:17 by nsalle      ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/19 13:49:49 by nsalle      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,17 +21,21 @@
 
 typedef struct      	s_player
 {
-	int					fd;
 	unsigned char		magic[4];
+	unsigned char		*code;
+	char				*pname;
 	char				name[PROG_NAME_LENGTH];
 	char				comment[COMMENT_LENGTH];
+	int					fd;
 	int					size;
+	int					start;
 }                   	t_player;
 
 typedef struct      	s_vm
 {
 	int					dump;
 	unsigned char		booldump;
+	unsigned char		arena[MEM_SIZE];
 	int					nb_player;
 	t_player			players[MAX_PLAYERS];
 }                   	t_vm;
@@ -40,5 +44,8 @@ void    all_checks(int argc, char **av, t_vm *vm);
 void	usage(void);
 int		get_size(t_vm *vm, int ip);
 void	check_realsize(t_vm *vm, int ip);
+void	get_code(t_vm *vm, int ip);
+
+void	init_arena(t_vm *vm);
 
 #endif
