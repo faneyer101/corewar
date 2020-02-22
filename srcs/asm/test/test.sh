fa5876b344dbd68e46dc9222f52874e2c0e1f303
Apple <apple@apples-mbp.home>
2020-02-23 06:16:31 +0700

    push asm

--- srcs/asm/test/test.sh
+++ srcs/asm/test/test.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-FILE = $1
+FILE = $1;
 
-xxd -a -g 1 corewar-unit-tests/tests/asm/valid/${FILE} > moitest
+xxd -a -g 1 $1 > moitest
 xxd -a -g 1 corewar-unit-tests/tests/asm/valid2/${FILE} > testbin
+
 diff -q moitest testbin
